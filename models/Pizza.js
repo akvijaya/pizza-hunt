0402dc39a030c8deeb453554e0ae3644d0cdd747
akvijaya <abisonvijay@gmail.com>
2021-12-19 22:45:09 -0500

    done replies

--- models/Pizza.js
+++ models/Pizza.js
@@ -37,10 +37,10 @@ const PizzaSchema = new Schema ({
 
 //get total count of comments and replies on retrieval 
 PizzaSchema.virtual('commentCount').get(function() {
-    return this.comments.length;
+    return this.comments.reduce((total, comment) => total + comment.replies.length + 1, 0);
 });
 
-//create pizza model using the pizza schema
+//create pizza model using   the pizza schema
 const Pizza = model('Pizza', PizzaSchema);
 
 //export the pizza model
